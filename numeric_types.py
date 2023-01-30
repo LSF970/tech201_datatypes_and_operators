e2e54bc8f9087f4bfcfc827ba40f074c5562b254
LSF970 <Lfairbrass@spartaglobal.com>
2023-01-30 12:22:10 +0000

    first commit

---
+++ numeric_types.py
@@ -0,0 +1,46 @@
+# Data types
+
+# Numeric - Ints, Floats, longs, complex
+# String - Text of any type
+# Boolean - True or False
+
+# Operators
+
+# Arithmetic operators
+
+# + , -, *, /
+
+# Comparison operators
+
+# >
+# <
+# ==
+# !=
+# >=
+# <=
+
+# Implementing numeric types
+
+# Ints
+
+a = 24
+b = 16
+
+print(a + b) # 40
+print(a > b) # True
+print(a < b) # False
+
+# Floats
+
+FloatNum = 1.356
+IntNum = 4
+
+print(FloatNum + IntNum) # 5.356
+
+# Theres no limit to the size of decimal, but Python will round things up
+
+one_third = 1 / 3
+print(one_third) # 0.33333333333
+
+print(one_third * 3) # 1.0
+
